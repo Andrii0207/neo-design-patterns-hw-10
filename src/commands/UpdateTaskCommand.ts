53dce85747d853a230355efc0b9248ccabcf45e6
Andrii Popov <97399846+Andrii0207@users.noreply.github.com>
2025-08-04 23:52:24 +0300

    finish project

--- src/commands/UpdateTaskCommand.ts
+++ src/commands/UpdateTaskCommand.ts
@@ -14,10 +14,16 @@ export class UpdateTaskCommand extends AbstractCommand {
   }
 
   execute(): void {
-    // TODO
+     const existing = this.taskList.getAllTasks().find(t => t.id === this.taskId);
+    if (existing) {
+      this.oldTask = { ...existing };
+      this.taskList.updateTask(this.taskId, this.updates);
+    }
   }
 
   undo(): void {
-    // TODO
+    if (this.oldTask) {
+      this.taskList.updateTask(this.taskId, this.oldTask);
+    }
   }
 }
